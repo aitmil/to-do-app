0b52392cc8cefb5e0074876b2d6cc185e48fba98
Anna Taranchuk <taranchuk4@yahoo.com>
2025-03-24 20:45:45 +0100

    feat: add QueryClientProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import Providers from "./providers";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "To Do App",
-  description: "To Do App",
+  description: "A simple ToDo app",
 };
 
 export default function RootLayout({
@@ -24,10 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
